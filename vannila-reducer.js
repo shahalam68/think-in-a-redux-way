b8effc147128517d01e8bebbae9008fa8c63aa3e
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2023-12-06 10:51:19 +0600

    last test

--- vannila-reducer.js
+++ vannila-reducer.js
@@ -1,6 +1,6 @@
 const array = [1, 2, 3, 4, 5];
 
 
-array.reduce((previousResult, currentValue) =>{
-    
-});
+console.log(array.reduce((previousResult, currentValue) => {
+    return previousResult + currentValue;
+}, 0));
