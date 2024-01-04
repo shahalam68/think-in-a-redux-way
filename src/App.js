0336e58a3012a94bdb1c6c2eeb2e53eddf2428e5
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-04 21:01:49 +0600

    create store

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Counter from "./components/Counter";
 
+import { Provider } from "react-redux";
+import store from "./redux/store";
+
 export default function App() {
     return (
-        <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
+        <Provider store={store} className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
             <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
                 Simple Counter Application
             </h1>
@@ -12,6 +15,6 @@ export default function App() {
                 <Counter />
                 <Counter />
             </div>
-        </div>
+        </Provider>
     );
 }
--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,29 +1,20 @@
-import { useState } from "react";
+import { connect } from "react-redux";
 
-export default function Counter() {
-    const [count, setCount] = useState(0);
-
-    const increment = () => {
-        setCount((prevCount) => prevCount + 1);
-    };
-
-    const decrement = () => {
-        setCount((prevCount) => prevCount - 1);
-    };
+ function Counter() {
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div className="text-2xl font-semibold">{count}</div>
+            <div className="text-2xl font-semibold">{}</div>
             <div className="flex space-x-3">
                 <button
                     className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={increment}
+                    onClick={}
                 >
                     Increment
                 </button>
                 <button
                     className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={decrement}
+                    onClick={}
                 >
                     Decrement
                 </button>
@@ -31,3 +22,9 @@ export default function Counter() {
         </div>
     );
 }
+
+const mapStateToProps = (state) => {
+
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Counter);
