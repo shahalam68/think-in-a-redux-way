adc5d5d1b3da1c8184a9a61f6ae7be010bd30799
A.K.M Shah Alam <a.k.mshahalam68@gmail.com>
2024-01-04 23:12:33 +0600

    p

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,20 +1,23 @@
-import { connect } from "react-redux";
 
- function Counter() {
+import { connect } from "react-redux";
+import { increment } from "../redux/counter/actions";
+import { decrement } from "../redux/counter/actions";
+
+ function Counter({count,increment,decrement}) {
 
     return (
         <div className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
-            <div className="text-2xl font-semibold">{}</div>
+            <div className="text-2xl font-semibold">{count}</div>
             <div className="flex space-x-3">
                 <button
                     className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
-                    onClick={}
+                    onClick={increment}
                 >
                     Increment
                 </button>
                 <button
                     className="bg-red-400 text-white px-3 py-2 rounded shadow"
-                    onClick={}
+                    onClick={decrement}
                 >
                     Decrement
                 </button>
@@ -23,8 +26,18 @@ import { connect } from "react-redux";
     );
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = (state,ownProps) => {
+    console.log(ownProps);
+    return{
+        count: state.value,
+    };
+}
 
+const mapDispatchToProps = (dispatch) => {
+    return{
+        increment:(value) => dispatch(increment(value)),
+        decrement:(value) => dispatch(decrement(value)),
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(Counter);
